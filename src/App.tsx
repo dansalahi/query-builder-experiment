96ae02638879e5b2445a24bc947d9a59f8efd6ab
daniel salahi <dan.salahii@gmail.com>
2023-07-09 09:46:57 +0300

    initail query builder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Field, formatQuery, QueryBuilder, RuleGroupType } from 'react-querybuilder';
+import 'react-querybuilder/dist/query-builder.css';
 
-function App() {
+const fields: Field[] = [
+  { name: 'firstName', label: 'First Name' },
+  { name: 'lastName', label: 'Last Name' },
+  {name: 'phone', label: 'Phone Number',inputType:'number'}
+];
+
+const initialQuery: RuleGroupType = { 
+  combinator: 'and',
+   rules: [
+  { field: 'firstName', operator: 'beginsWith', value: 'Daniel' },
+  { field: 'lastName', operator: 'in', value: 'Salahi' },
+  {field :'phone', operator:'=', value:1664,}
+] };
+
+const App= () => {
+
+  const [query, setQuery] = useState<RuleGroupType>(initialQuery);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryBuilder 
+      controlClassnames={{ addRule: 'bold' }}
+       fields={fields} 
+      query={query} 
+      onQueryChange={q => setQuery(q)} />
+      <h4>
+        JSONLogic as result of <code>formatQuery(query, 'jsonlogic')</code>:
+      </h4>
+      <pre>{JSON.stringify(formatQuery(query, "jsonlogic"), null, 2)}</pre>
     </div>
   );
 }
